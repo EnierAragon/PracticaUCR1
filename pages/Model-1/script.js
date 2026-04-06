a03eeebfd2c2eb67a0a64a5e95d669f116172dc3
JoshuaAE12 <joshuabenjasolanojimenes@gmail.com>
2026-04-06 16:37:24 -0600

    final del reto1

--- pages/Model-1/script.js
+++ pages/Model-1/script.js
@@ -1,1 +1,26 @@
-fetch();
+document.addEventListener("DOMContentLoaded", () => {
+
+    fetch("datos.json")
+        .then(response => response.json())
+        .then(datos => {
+            const tbody = document.querySelector("#miTabla tbody");
+            tbody.innerHTML = "";
+
+            datos.forEach(persona => {
+                const fila = document.createElement("tr");
+
+                fila.innerHTML = `
+                    <td>${persona.id}</td>
+                    <td>${persona.nombre}</td>
+                    <td>${persona.provincia}</td>
+                    <td>${persona.correo}</td>
+                `;
+
+                tbody.appendChild(fila);
+            });
+        })
+        .catch(error => {
+            console.error("Error cargando el JSON:", error);
+        });
+
+});
